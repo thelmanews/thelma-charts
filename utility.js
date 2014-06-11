25fa036d2d4d7e27a4d73e40eabd6ac1a3ae037d
Sepand Ansari <sepand@worksmartlabs.com>
2014-06-11 18:00:27 -0400

    cleaning up vertical, adding responsiveness to data and size

--- utility.js
+++ utility.js
@@ -1,9 +1,6 @@
 var Thelma = window.Thelma || {};
 
-Thelma.utilities = {
-
-	
-
+Thelma.util = {
 
 }
 
@@ -11,26 +8,26 @@ Thelma.utilities = {
 Thelma.chartValidation = {
 
 	errors : [],
-	validateChartData: function(polymerObject) {
-		// polymerObject is passed to the function to make chartValidation not dependant on polymer
+	validateChartData: function(polymerObj) {
+		// polymerObj is passed to the function to make chartValidation not dependant on polymer
 		
 		console.log(' ---- chart validation ---- ')
 
-		var errors = polymerObject.errors;
+		var errors = polymerObj.errors;
 		var i = 0;
-		if(!polymerObject.chartData) {
+		if(!polymerObj.chartData) {
 			errors.push('no chart data');
 		}
-		errors = errors.concat(polymerObject.chartSpecificDataValidate());
+		errors = errors.concat(polymerObj.chartSpecificDataValidate());
 		console.log(errors);
 
 		if(errors.length>0) {
-			polymerObject.$.chart.style.opacity = 0.5; // this is for testing
-			polymerObject.$.data_errors.style.display = 'block'; // this is for testing
+			polymerObj.$.chart.style.opacity = 0.5; // this is for testing
+			polymerObj.$.data_errors.style.display = 'block'; // this is for testing
 			
 			for (i; i < errors.length; i++){	
-				polymerObject.$.data_errors.appendChild(document.createElement('li')).innerHTML = errors[i].msg; // this is for testing
-				polymerObject.asyncFire('error', errors[i]); // where error is an object containing details
+				polymerObj.$.data_errors.appendChild(document.createElement('li')).innerHTML = errors[i].msg; // this is for testing
+				polymerObj.asyncFire('error', errors[i]); // where error is an object containing details
 			}
 		}
 		
@@ -42,5 +39,54 @@ Thelma.chartValidation = {
 	}
 
 
+
+}
+
+Thelma.chartUtils = {
+
+	setupDimensions: function(polymerObj) {
+		var dims = {};
+
+		//TODO there needs to be height and visHeight?! later for drawing area (excluding labels and axis)?!
+		dims.margin = {
+		          top : 16,
+		          right : 0,
+		          bottom : 16,
+		          left : 0,
+		          label: 3
+		      }, 
+	    dims.width = polymerObj.chartWidth*0.95 - dims.margin.left - dims.margin.right, 
+	    dims.height = polymerObj.chartHeight*0.55 - dims.margin.top - dims.margin.bottom,
+	    dims.barWidth = dims.width* 0.12 ,
+	    dims.textLabelMargin = dims.height*0.05;
+
+	    return dims;
+
+	},
+	/*
+	 *	builds simple x,y scales for charts. xAccessFun and yAccessFun are optional accessor functions.
+	*/ 
+	simpleScaleBuilder: function(width, height, chartData, xAccessFun, yAccessFun) {
+
+		  var scales = {};
+		  xAccessFun = xAccessFun || function(d) {return d.label}; 
+		  yAccessFun = yAccessFun || function(d) {return d.value}; 
+		  
+		  scales.x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+	      scales.y = d3.scale.linear().range([0, height]);
+
+	      var max = d3.max(this.chartData, yAccessFun);
+
+	      scales.y.domain([0, max]); 
+	      scales.x.domain(this.chartData.map(xAccessFun));
+
+	      scales.colors = d3.scale.category10();
+		      
+		  scales.colors.domain(this.chartData.length);
+
+
+	      return scales;
+
+	}
 
 }
